6ec044f9d8b3ec7a2983428ac5f6f0b2142fe26f
Toraudonn <toraudonn@gmail.com>
2022-05-09 14:17:42 +0000

    remove older versions

--- pyEdgeEval/info.py
+++ pyEdgeEval/info.py
@@ -4,7 +4,7 @@
 Information about this module
 """
 
-__version__ = "0.0.2"
+__version__ = "0.0.1"
 __author__ = "haruishi43"
 __email__ = "toraudonn@gmail.com"
 __description__ = "Python Edge Evaluation Benchmark"
