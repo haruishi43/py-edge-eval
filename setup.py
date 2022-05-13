3d3c244b40c637f5e1596679fdeeaaf980dd15bb
Toraudonn <toraudonn@gmail.com>
2022-05-13 13:31:53 +0000

    fix setup script

--- setup.py
+++ setup.py
@@ -138,9 +138,9 @@ setup(
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     python_requires=">=3.6",
     keywords=[
